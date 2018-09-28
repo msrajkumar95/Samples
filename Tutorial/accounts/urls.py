a0becc2cd67804205b7086ce342bcf37274516bc
Rajkumar Srinivasan <msrajkumar95@gmail.com>
2018-09-28 10:01:24 +0530

    Updated

--- Tutorial/accounts/urls.py
+++ Tutorial/accounts/urls.py
@@ -1,6 +1,10 @@
-from django.urls import re_path
-from . import views
+from django.urls import path
+from accounts import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    re_path('^$', views.home),
+    path('', views.home, name = 'home'),
+    path('register/', views.register, name = 'register'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html')),
+    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html')),
 ]
