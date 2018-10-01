45da439dcdd6239acc4a81b650b37f939225fce6
Rajkumar Srinivasan <msrajkumar95@gmail.com>
2018-10-01 16:08:21 +0530

    Update views.py

--- Tutorial/accounts/views.py
+++ Tutorial/accounts/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, redirect
 from accounts.forms import RegistrationForm, EditProfileForm
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required
 def home(request):
     return render(request, 'accounts/home.html')
 
@@ -19,13 +22,15 @@ def register(request):
         args = {'form': form}
         return render(request, 'accounts/register.html', args)
     
+@login_required
 def view_profile(request):
     args = {'user': request.user}
     return render(request, 'accounts/profile.html', args)
 
+@login_required
 def edit_profile(request):
     if request.method == 'POST':
-        form = EditProfileForm(request.POST, instance = request.user)
+        form = EditProfileForm(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
             return redirect('/account/profile')
@@ -35,9 +40,10 @@ def edit_profile(request):
         args = {'form': form}
         return render(request, 'accounts/edit_profile.html', args)
     
+@login_required
 def change_password(request):
     if request.method == 'POST':
-        form = PasswordChangeForm(data = request.POST, user = request.user)
+        form = PasswordChangeForm(data=request.POST, user=request.user)
         if form.is_valid():
             form.save()
             update_session_auth_hash(request, form.user)
