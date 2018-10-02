5277284d45aa09460e008e4fa70c334e5ede717b
Rajkumar Srinivasan <msrajkumar95@gmail.com>
2018-10-02 21:10:28 +0530

    Add files via upload

--- Tutorial/accounts/urls.py
+++ Tutorial/accounts/urls.py
@@ -3,15 +3,16 @@ from accounts import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.home, name = 'home'),
-    path('register/', views.register, name = 'register'),
-    path('profile/', views.view_profile, name = 'view_profile'),
-    path('profile/edit/', views.edit_profile, name = 'edit_profile'),
-    path('change-password/', views.change_password, name = 'change_password'),
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html')),
-    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html')),
-    path('reset-password/', auth_views.PasswordResetView.as_view(), name='reset-password'),
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.view_profile, name='view_profile'),
+    path('profile/edit/', views.edit_profile, name='edit_profile'),
+    path('change-password/', views.change_password, name='change_password'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+    path('reset-password/', auth_views.PasswordResetView.as_view(template_name='accounts/reset_password.html', success_url='accounts:password_reset_done'), name='reset_password'),
     path('reset-password/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset-password/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    re_path(r'reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    re_path(r'reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/', auth_views.PasswordResetConfirmView.as_view(success_url='accounts:password_reset_complete'), name='password_reset_confirm'),
 ]
+# {'post_reset_redirect': 'accounts:password_reset_done'}, 
--- Tutorial/accounts/views.py
+++ Tutorial/accounts/views.py
@@ -1,12 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from accounts.forms import RegistrationForm, EditProfileForm
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
-from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-@login_required
 def home(request):
     return render(request, 'accounts/home.html')
 
@@ -15,41 +13,38 @@ def register(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/account')
+            return redirect(reverse('accounts:home'))
     else:
         form = RegistrationForm()
         
         args = {'form': form}
         return render(request, 'accounts/register.html', args)
-    
-@login_required
+
 def view_profile(request):
     args = {'user': request.user}
     return render(request, 'accounts/profile.html', args)
 
-@login_required
 def edit_profile(request):
     if request.method == 'POST':
         form = EditProfileForm(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
-            return redirect('/account/profile')
+            return redirect(reverse('accounts:view_profile'))
     else:
         form = EditProfileForm(instance = request.user)
         
         args = {'form': form}
         return render(request, 'accounts/edit_profile.html', args)
     
-@login_required
 def change_password(request):
     if request.method == 'POST':
         form = PasswordChangeForm(data=request.POST, user=request.user)
         if form.is_valid():
             form.save()
             update_session_auth_hash(request, form.user)
-            return redirect('/account/profile')
+            return redirect(reverse('accounts:view_profile'))
         else:
-            return redirect('/account/change-password')
+            return redirect(reverse('accounts:change_password'))
     else:
         form = PasswordChangeForm(user = request.user)
         
