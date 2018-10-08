fb41e566e18e15febcd3f3c3ac9595befda60455
Rajkumar Srinivasan <msrajkumar95@gmail.com>
2018-10-08 16:16:52 +0530

    Update urls.py

--- Tutorial/home/urls.py
+++ Tutorial/home/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from home import views
 
 # app_name = 'home'
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home')
+    path('', views.HomeView.as_view(), name='home'),
+    re_path(r'friend/(?P<operation>.+)/(?P<pk>\d+)/', views.change_friends, name='change_friend')
 ]
