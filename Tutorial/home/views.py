0723feab6da34aebc441e5e20a648c5dab7872c8
Rajkumar Srinivasan <msrajkumar95@gmail.com>
2018-10-08 16:16:34 +0530

    Update views.py

--- Tutorial/home/views.py
+++ Tutorial/home/views.py
@@ -1,8 +1,9 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 from home.forms import HomeForm
-from home.models import Post
+from home.models import Post, Friend
 
 class HomeView(TemplateView):
     template_name = 'home/home.html'
@@ -10,8 +11,11 @@ class HomeView(TemplateView):
     def get(self, request):
         form = HomeForm()
         posts = Post.objects.all().order_by('-created')
+        users = User.objects.exclude(id=request.user.id)
+        friend = Friend.objects.get(current_user=request.user)
+        friends = friend.users.all()
         
-        args = {'form': form, 'posts': posts}
+        args = {'form': form, 'posts': posts, 'users': users, 'friends': friends}
         return render(request, self.template_name, args)
     
     def post(self, request):
@@ -27,3 +31,12 @@ class HomeView(TemplateView):
             
         args = {'form': form, 'text': text}
         return render(request, self.template_name, args)
+    
+def change_friends(request, operation, pk):
+    friend = User.objects.get(pk=pk)
+    if operation == 'add':
+        Friend.make_friend(request.user, friend)
+    elif operation == 'remove':
+        Friend.lose_friend(request.user, friend)
+        
+    return redirect('home:home')
