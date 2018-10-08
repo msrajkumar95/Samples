2ff8ca1b921dc8a2b22881c80465b107124634c5
Rajkumar Srinivasan <msrajkumar95@gmail.com>
2018-10-08 16:15:28 +0530

    Update urls.py

--- Tutorial/accounts/urls.py
+++ Tutorial/accounts/urls.py
@@ -7,6 +7,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('profile/', views.view_profile, name='view_profile'),
+    re_path(r'profile/(?P<pk>\d+)/', views.view_profile, name='view_profile_with_pk'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
     path('change-password/', views.change_password, name='change_password'),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
