dc6755a17f32230744c6fb9b12bfa84666786607
Rajkumar Srinivasan <msrajkumar95@gmail.com>
2018-10-10 15:13:43 +0530

    Update urls.py

--- Tutorial/accounts/urls.py
+++ Tutorial/accounts/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('profile/', views.view_profile, name='view_profile'),
     re_path(r'profile/(?P<pk>\d+)/', views.view_profile, name='view_profile_with_pk'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
-    path('change-password/', views.change_password, name='change_password'),
+    path('profile/password/', views.change_password, name='change_password'),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
     path('reset-password/', auth_views.PasswordResetView.as_view(template_name='accounts/reset_password.html', email_template_name='accounts/reset_password_email.html', success_url=reverse_lazy('accounts:password_reset_done')), name='reset_password'),
