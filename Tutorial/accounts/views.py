dd9cf80dbc06dfa1c71e1f7d9c761c4b693de1e0
Rajkumar Srinivasan <msrajkumar95@gmail.com>
2018-10-10 15:11:34 +0530

    Update views.py

--- Tutorial/accounts/views.py
+++ Tutorial/accounts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, reverse
 from accounts.forms import RegistrationForm, EditProfileForm
-from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth import update_session_auth_hash, authenticate, login
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 
@@ -11,20 +12,24 @@ def register(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            print('redirect something',redirect(reverse('home:home')),reverse('home:home'))
+            username = request.POST.get('username')
+            password = request.POST.get('password1')
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
             return redirect(reverse('home:home'))
     else:
         form = RegistrationForm()
         
-        args = {'form': form}
-        return render(request, 'accounts/register.html', args)
+    args = {'form': form}
+    return render(request, 'accounts/register.html', args)
 
 def view_profile(request, pk = None):
+    storage = messages.get_messages(request)
     if pk:
         user = User.objects.get(pk=pk)
     else:
         user = request.user
-    args = {'user': user}
+    args = {'user': user, 'messages': storage}
     return render(request, 'accounts/profile.html', args)
 
 def edit_profile(request):
@@ -44,12 +49,11 @@ def change_password(request):
         form = PasswordChangeForm(data=request.POST, user=request.user)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Your password has been changed successfully.')
             update_session_auth_hash(request, form.user)
             return redirect(reverse('accounts:view_profile'))
-        else:
-            return redirect(reverse('accounts:change_password'))
     else:
         form = PasswordChangeForm(user = request.user)
         
-        args = {'form': form}
-        return render(request, 'accounts/change_password.html', args)
+    args = {'form': form}
+    return render(request, 'accounts/change_password.html', args)
