d6f466b36f5cad44ceed1cc7b70ef307c31ca691
Rajkumar Srinivasan <msrajkumar95@gmail.com>
2018-10-11 09:15:42 +0530

    Update models.py

--- Tutorial/accounts/models.py
+++ Tutorial/accounts/models.py
@@ -24,6 +24,6 @@ class UserProfile(models.Model):
     
 def create_profile(sender, **kwargs):
     if kwargs['created']:
-        user_profile = UserProfile.objects.create(user=kwargs['instance'])
+        UserProfile.objects.create(user=kwargs['instance'])
         
 post_save.connect(create_profile, sender=User)
